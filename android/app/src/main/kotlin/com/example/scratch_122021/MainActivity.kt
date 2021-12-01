f19fe785c66538164012d66e94611df848fe6374
Catyousha <jakaasha@gmail.com>
2021-12-01 15:50:28 +0700

    bisa ngoding != ngoding yang bener

---
+++ android/app/src/main/kotlin/com/example/scratch_122021/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.scratch_122021
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Scratch122021());
+}
+
+class Scratch122021 extends StatelessWidget {
+  const Scratch122021({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Scratch 122021',
+      home: Center(
+        child: Text("P"),
+      ),
+    );
+  }
+}
